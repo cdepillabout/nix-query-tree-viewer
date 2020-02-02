09dfcde4f8207d6ab42d85933427ad89370dd430
Dennis Gosnell <cdep.illabout@gmail.com>
2020-02-02 16:35:54 +0900

    update

--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,3 @@
-
 use gio::prelude::*;
 use glib::clone;
 use gtk::prelude::*;
@@ -37,10 +36,29 @@ fn render_tree_view(builder: gtk::Builder, nix_store_res: &NixStoreRes) {
     tree_view.append_column(&column);
 }
 
+fn show_msg_in_statusbar(builder: gtk::Builder, msg: &str) {
+    let statusbar: gtk::Statusbar = builder.get_object("statusbar").unwrap();
+    statusbar.remove_all(0);
+    statusbar.push(0, msg);
+}
+
 fn render_nix_store_err(builder: gtk::Builder, nix_store_path: &Path, nix_store_err: &NixStoreErr) {
-    let error_dialog: gtk::MessageDialog =
-        builder.get_object("errorDialog").unwrap();
-    error_dialog.set_property_secondary_text(Some(&format!("Error running `nix-store --query --tree {}`:\n\n{}", nix_store_path.to_string_lossy(), nix_store_err)));
+    let error_dialog: gtk::MessageDialog = builder.get_object("errorDialog").unwrap();
+    let error_msg = &format!(
+        "Error running `nix-store --query --tree {}`:\n\n{}",
+        nix_store_path.to_string_lossy(),
+        nix_store_err
+    );
+    error_dialog.set_property_secondary_text(Some(error_msg));
+    error_dialog.run();
+    error_dialog.destroy();
+    show_msg_in_statusbar(
+        builder,
+        &format!(
+            "Error running `nix-store --query --tree {}`",
+            nix_store_path.to_string_lossy()
+        ),
+    );
 }
 
 fn render_nix_store_res(builder: gtk::Builder, nix_store_res: &ExecNixStoreRes) {
