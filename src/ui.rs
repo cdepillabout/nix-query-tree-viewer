4160b322913bce3ab0644b9fdd29e4abf747d21a
Dennis Gosnell <cdep.illabout@gmail.com>
2020-02-08 15:58:43 +0900

    update

--- src/ui.rs
+++ src/ui.rs
@@ -5,27 +5,12 @@ mod stack;
 
 pub mod prelude;
 
-use glib::clone;
 use std::path::Path;
 use std::rc::Rc;
 
 use super::nix_query_tree::exec_nix_store::{ExecNixStoreRes, NixStoreErr};
 
 use prelude::*;
-
-fn connect_menu_buttons(app: gtk::Application, builder: gtk::Builder) {
-    let about_menu_item: gtk::MenuItem = builder.get_object_expect("aboutMenuItem");
-    let about_dialog: gtk::AboutDialog = builder.get_object_expect("aboutDialog");
-    about_menu_item.connect_activate(move |_| {
-        about_dialog.run();
-        about_dialog.hide();
-    });
-
-    let quit_menu_item: gtk::MenuItem = builder.get_object_expect("quitMenuItem");
-    quit_menu_item.connect_activate(clone!(@weak app => move |_| {
-        app.quit();
-    }));
-}
 
 fn render_nix_store_err(builder: gtk::Builder, nix_store_path: &Path, nix_store_err: &NixStoreErr) {
     let error_dialog: gtk::MessageDialog = builder.get_object_expect("errorDialog");
