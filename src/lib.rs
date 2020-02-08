6025656fdeb9afcf9d7c8aa57d7d03b30546d801
Dennis Gosnell <cdep.illabout@gmail.com>
2020-02-08 15:25:51 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,6 @@ pub mod tree;
 mod opts;
 mod ui;
 
-extern crate gio;
-extern crate gtk;
-
 pub fn default_main() {
     // nix-store --query --tree /nix/store/jymg0kanmlgbcv35wxd8d660rw0fawhv-hello-2.10.drv
     // nix-store --query --tree /nix/store/qy93dp4a3rqyn2mz63fbxjg228hffwyw-hello-2.10
